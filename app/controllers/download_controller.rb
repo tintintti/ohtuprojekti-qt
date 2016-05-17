d8101e0062810ce70a994aa4496c6d2a7496f277
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-05-17 15:23:25 +0300

    file path fixed

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,6 +1,6 @@
 class DownloadController < ApplicationController
   def download
-    file = File.open("/home/ahjyrkia/qt-software-project/app/assets/textdata/theFile.txt", "rb")
+    file = File.open(File.join(Rails.root, 'app/assets/textdata/theFile.txt'), "rb")
     contents = file.read
     send_data(contents, filename:"theFile.txt")
   end
