d413f37312db68beb7cd3e0f07d07adab42f4ccf
Student Laptop User <fuksi@dhcp-asv-116.helsinki.fi>
2016-05-19 15:12:59 +0300

    merge problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   get 'file' => 'download#download'
 
-  get 'charts' => 'welcome#index'
+  get 'charts' => 'welcome#view'
+
+  root 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
