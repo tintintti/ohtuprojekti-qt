09c2428c9ba2503c2e51967774ae683297b43c0c
Student Laptop User <fuksi@dhcp-asv-116.helsinki.fi>
2016-05-19 12:11:02 +0300

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
   get 'file' => 'download#download'
+
+  get 'charts' => 'welcome#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
