e1d3fb9b057cd12198385e567f44dd10f4bd7eb5
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-05-25 14:20:36 +0300

    moved getting users by email from downloadcontroller to user_handler

--- app/classes/forum_handlers/user_handler.rb
+++ app/classes/forum_handlers/user_handler.rb
@@ -22,5 +22,27 @@ class UserHandler
     arr
   end
 
+  def self.users_by_email_provider
+    users = User.all
+    emails = {}
+    users.each do |user|
+      email = user.email
+      # if user has removed their account, email is nil
+      if email == nil
+        next
+      end
+      email = email.split("@")
+      email = email[1]
+      if !emails.key? email
+        emails[email] = {label:email, value:0, users:[]}
+      end
+      emails[email][:value] += 1
+      emails[email][:users] << {user: user.username, slug: user.userslug}
+    end
+    data = []
+    emails.each_value { |value| data << value }
+    sorted_data = data.sort_by { |item| item[:value] }
+  end
+
 
 end
