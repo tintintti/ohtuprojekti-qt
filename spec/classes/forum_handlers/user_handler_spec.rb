9edf64495f1a808f4473681c0ff7e0406c1eee5d
Mikko Markkinen <mikko.markkinen@gmail.com>
2016-05-25 14:11:29 +0300

    spec/classes and userhandler test file created

---
+++ spec/classes/forum_handlers/user_handler_spec.rb
@@ -0,0 +1,10 @@
+require Rails.root.to_s + '/app/classes/forum_handlers/user_handler.rb'
+
+
+RSpec.describe UserHandler do
+
+  it "user_postcounts returns an array" do
+    expect(UserHandler.user_postcounts).to be_instance_of(Array)
+  end
+
+end
