b4dce1d1419b25a0880c86307a1f132c3676db3f
Ola Lansman <ola.lansman@helsinki.fi>
2016-05-25 11:39:35 +0300

    New RSpecs for Post, User and Topic

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :post do
     pid 1
-    did 1
+    uid 1
     tid 1
     content "MyString"
-    timestamp 1
+    timestamp "a lot"
     reputation 1
     votes 1
-    edited 1
+    edited "stringToo"
     deleted false
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "user is created correctly" do
+    it 'creates user with correct parameters' do
+      params = DatabaseHelpers.get_posts
+      post = Post.create(
+          pid:params["pid"],
+          uid:params["uid"],
+          tid:params["tid"],
+          content:params["content"],
+          timestamp:params["timestamp"],
+          reputation:params["reputation"],
+          votes:params["votes"],
+          edited:params["edited"],
+          deleted:params["deleted"],
+          index:params["index"]
+      )
+
+      expect(post).to be_valid
+      expect(post.pid).to eq(329490)
+      expect(post.uid).to eq(20121)
+      expect(post.tid).to eq(67471)
+      expect(post.content).to eq("<p>how QList store data in memory? like list or array?<br />\nthank you!</p>\n")
+      expect(post.timestamp).to eq("1464159358385")
+      expect(post.reputation).to eq(0)
+      expect(post.votes).to eq(0)
+      expect(post.edited).to eq("0")
+      expect(post.deleted).to eq(false)
+      expect(post.index).to eq(0)
+    end
+
+  end
 end
