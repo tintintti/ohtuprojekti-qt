909513bb45608598971e050ed3aee4ca92df0a4d
Ola Lansman <ola.lansman@helsinki.fi>
2016-05-30 13:58:13 +0300

    Added gitstats as demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def authors
+    render "test/authors"
+  end
+
+  def index
+    render "test/index"
+  end
+
+  def activity
+    render "test/activity"
+  end
+
+  def files
+    render "test/files"
+  end
+
+  def lines
+    render "test/lines"
+  end
+
+  def tags
+    render "test/tags"
+  end
+
 end
