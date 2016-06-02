cd71166c47c11dcdeebc2b86c921e7a63a927416
Ola Lansman <ola.lansman@helsinki.fi>
2016-06-02 12:56:20 +0300

    safety.commit

--- lib/parse_git_stats.rb
+++ lib/parse_git_stats.rb
@@ -2,15 +2,19 @@ require 'httparty'
 
 class ParseGitStats
   def self.create_everything_required_for_gitstats
-    all_repositories = HTTPary.get("https://api.github.com/users/qtproject/repos?per_page=128").parsed_response
+    all_repositories = HTTParty.get("https://api.github.com/users/qtproject/repos?per_page=128").parsed_response
+    i = 0
     all_repositories.each do |repo|
+      i += 1
       # git clone repo
       load_git_stuff repo
-      create_and_move_stats_to_views
-      add_commits_to_database
-      somehow_manage_adding_new_routes
+      # create_and_move_stats_to_views
+      # add_commits_to_database
+      # somehow_manage_adding_new_routes
       # rm repo and gitstats
-      break
+      puts i
+      puts "/"
+      puts all_repositories.count
     end
 
   end
@@ -30,6 +34,9 @@ class ParseGitStats
   def self.load_git_stuff repo
     `git clone #{repo["clone_url"]}`
     repo_name = repo["name"]
-    `cd #{repo_name} ; ls -a | grep -v .git | xargs rm -rf`
+    `cd #{repo_name} ; ls | grep -v .git | xargs rm -rf`
+    `mv #{repo_name} qt-projects/`
   end
 end
+
+repo = GitStats::GitData::Repo.new(path: 'qt-projects/qt-labs-umlquick', first_commit_sha: '72dd7b21734ed80e3f3670a7e5c85a298a73e5a1', last_commit_sha: 'HEAD')
