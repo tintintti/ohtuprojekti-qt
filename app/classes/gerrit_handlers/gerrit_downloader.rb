2dacceef3dbac027c28937dc3b0d878023d268ad
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-06-07 15:40:02 +0300

    class for handling owner data and method for getting change amounts by owner

--- app/classes/gerrit_handlers/gerrit_downloader.rb
+++ app/classes/gerrit_handlers/gerrit_downloader.rb
@@ -2,16 +2,29 @@ class GerritDownloader
   include HTTParty
   base_uri 'https://codereview.qt-project.org'
 
+  # returns a hash
   def projects
     projects = body_no_first_line(self.class.get("/projects/"))
     JSON.parse(projects)
   end
 
+  # returns an array
   def changes
     change = body_no_first_line(self.class.get("/changes/"))
     JSON.parse(change)
   end
 
+  def n_changes(amount)
+    change = body_no_first_line(self.class.get("/changes/?n=#{amount}"))
+    JSON.parse(change)
+  end
+
+  def n_changes_with_status(amount, status)
+      change = body_no_first_line(self.class.get("/changes/?q=status:#{status}&n=#{amount}"))
+      JSON.parse(change)
+  end
+
+  # returns a hash
   def change(id)
     details = body_no_first_line(self.class.get("/changes/#{id}/detail"))
     JSON.parse(details)
