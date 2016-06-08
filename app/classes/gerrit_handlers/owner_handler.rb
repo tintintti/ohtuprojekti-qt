1765754fe66235f69b3c938513b62832cb732ca8
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-06-08 18:09:33 +0300

    refactoring

--- app/classes/gerrit_handlers/owner_handler.rb
+++ app/classes/gerrit_handlers/owner_handler.rb
@@ -3,27 +3,37 @@ class OwnerHandler
 
   def self.changes_by_owner
       changes = @downloader.changes
-      change_details = []
-      changes.each do |change|
-        details = @downloader.change(change['id'])
-        if details != nil
-          change_details << details
-        end
-      end
+      details = fetch_details(changes)
 
-      owners = {}
-      change_details.each do |change|
-        owner_id = change['owner']['_account_id']
-        if !owners.key?(owner_id)
-          owners[owner_id] = {owner: change['owner'], changes: 0}
-        end
-        owners[owner_id][:changes] += 1
-      end
       data = []
-      owners.each_value do |v|
+      owners(details).each_value do |v|
         data << {label: v[:owner]['name'], value: v[:changes]}
       end
       sorted_data = data.sort_by { |item| item[:value] }
+  end
+
+  def self.owners(change_details)
+    owners = {}
+    change_details.each do |change|
+      owner_id = change['owner']['_account_id']
+      if !owners.key?(owner_id)
+        owners[owner_id] = {owner: change['owner'], changes: 0}
+      end
+      owners[owner_id][:changes] += 1
+    end
+    owners
+  end
+
+  def self.fetch_details(changes)
+    change_details = []
+
+    changes.each do |change|
+      details = @downloader.change(change['id'])
+      if details != nil
+        change_details << details
+      end
+    end
+    change_details
   end
 
 end
