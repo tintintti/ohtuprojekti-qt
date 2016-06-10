3458d3103b42e9185b41102a09ee2147c6ade32e
TaikaMurmeli <sami.sarsa@helsinki.fi>
2016-06-10 15:01:47 +0300

    drawing git chart empties previous charts

--- app/assets/javascripts/gitCharts.js
+++ app/assets/javascripts/gitCharts.js
@@ -1,6 +1,7 @@
 // var commits = $('#git_data').data().commits;
 var authors = $("#git_data").data().authors;
 function drawAuthorPieChart() {
+    emptyCharts();
     addTitle("#charts", "pieChartTitle", "Commitit vuoden 2015 alusta nykyhetkeen");
     drawPieChart("authors", authors, true, "#charts");
 }
