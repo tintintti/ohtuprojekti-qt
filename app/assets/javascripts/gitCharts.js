1995db8518c26c156c08b94cc6a0015130c6e3ac
TaikaMurmeli <sami.sarsa@helsinki.fi>
2016-06-10 13:56:54 +0300

    titles for git charts, still needs data in heroku

--- app/assets/javascripts/gitCharts.js
+++ app/assets/javascripts/gitCharts.js
@@ -1,6 +1,6 @@
 // var commits = $('#git_data').data().commits;
 var authors = $("#git_data").data().authors;
 function drawAuthorPieChart() {
-    addTitle("#charts", "pieChartTitle", "asdasdlolkebab");
+    addTitle("#charts", "pieChartTitle", "Commitit vuoden 2015 alusta nykyhetkeen");
     drawPieChart("authors", authors, true, "#charts");
 }
