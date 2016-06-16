c3bcde8a014d9f49467ff2a166b17f6e3b8b05a6
Ola Lansman <ola.lansman@helsinki.fi>
2016-06-16 14:08:45 +0300

    More data to the test data base

--- app/classes/gerrit_handlers/owner_handler.rb
+++ app/classes/gerrit_handlers/owner_handler.rb
@@ -11,28 +11,28 @@ class OwnerHandler
       sorted_data = data.sort_by { |item| item[:value] }
   end
 
-  # def self.owners(change_details)
-  #   owners = {}
-  #   change_details.each do |change|
-  #     owner_id = change['owner']['_account_id']
-  #     if !owners.key?(owner_id)
-  #       owners[owner_id] = {owner: change['owner'], changes: 0}
-  #     end
-  #     owners[owner_id][:changes] += 1
-  #   end
-  #   owners
-  # end
-  #
-  # def self.fetch_details(changes)
-  #   change_details = []
-  #
-  #   changes.each do |change|
-  #     details = @downloader.change(change['id'])
-  #     if details != nil
-  #       change_details << details
-  #     end
-  #   end
-  #   change_details
-  # end
+  def self.owners(change_details)
+    owners = {}
+    change_details.each do |change|
+      owner_id = change['owner']['_account_id']
+      if !owners.key?(owner_id)
+        owners[owner_id] = {owner: change['owner'], changes: 0}
+      end
+      owners[owner_id][:changes] += 1
+    end
+    owners
+  end
+
+  def self.fetch_details(changes)
+    change_details = []
+
+    changes.each do |change|
+      details = @downloader.change(change['id'])
+      if details != nil
+        change_details << details
+      end
+    end
+    change_details
+  end
 
 end
