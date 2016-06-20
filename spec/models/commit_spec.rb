7ab0b4bebc221ea60bfa68f7c9e74589612eb834
Ola Lansman <ola.lansman@helsinki.fi>
2016-06-20 13:50:54 +0300

    Removed extra stuff from parse_git_stats

--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Commit, type: :model do
   it 'is valid' do
     commit = FactoryGirl.create(:commit)
     expect(commit).to be_valid
+    puts "Commit count is " + Commit.all.count.to_s
     end
 end
