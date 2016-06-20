6b4bb80a620a2853980ab1465a97ea7d8ed51853
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-06-20 11:50:29 +0300

    removed autogenerated tests for unused classes and added some tests for change handler

--- spec/classes/gerrit_handlers/change_handler_spec.rb
+++ spec/classes/gerrit_handlers/change_handler_spec.rb
@@ -50,4 +50,33 @@ RSpec.describe ChangeHandler do
     expect(ChangeHandler.revisions_needed_to_pass.first).to have_key(:value)
   end
 
+  it "change_data includes keys :times and :revisions" do
+    expect(ChangeHandler.change_data).to have_key(:times)
+    expect(ChangeHandler.change_data).to have_key(:revisions)
+  end
+
+  it "change_data returns a hash with arrays as values" do
+    data = ChangeHandler.change_data
+    expect(data[:times]).to     be_instance_of(Array)
+    expect(data[:revisions]).to be_instance_of(Array)
+  end
+
+  it "averages includes keys :time and :revisions" do
+    expect(ChangeHandler.averages).to have_key(:time)
+    expect(ChangeHandler.averages).to have_key(:revisions)
+  end
+
+  it "average_time returns 0 if there are time_to_pass is empty" do
+    ChangeHandler.stubs(:time_to_pass).returns([])
+    expect(ChangeHandler.average_time).to be(0)
+  end
+
+  it "average_time returns the average of the times got from time_to_pass" do
+    value1 = 200
+    value2 = 400
+    ChangeHandler.stubs(:time_to_pass).returns([{label: 1, value: value1}, {label: 2, value: value2}])
+    expect(ChangeHandler.average_time).to be((value1 + value2) / 2)
+  end
+
+
 end
